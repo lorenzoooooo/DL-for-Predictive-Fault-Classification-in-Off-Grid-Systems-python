c37f8f97903090a2a67c04fee1af968d3a36e7de
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-04-01 17:24:27 +0200

    update

--- K-folding.py
+++ K-folding.py
@@ -15,24 +15,16 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Bidirectional
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import cross_val_score
+# from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import StratifiedKFold
-# from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.optimizers import Adam
-# from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import MultiLabelBinarizer
+
 ''' load '''
 dataset_path=r'C:\\Users\\UTENTE\\Desktop\\py\\risultati_int\\t13008_t16399_t1059_t1021\\mincellvoltage_panelpower\\1_1_1_3_0.25\\'
 RESULT=pickle.load(open(dataset_path+'dataset.pkl','rb'))
 X=RESULT['X']
 Y=RESULT['Y']
 path=RESULT['path']
-
-# encoder = LabelEncoder()
-# encoder.fit(Y)
-# Y = encoder.transform(Y)
-# Y=MultiLabelBinarizer().fit_transform(Y.numpy())
 
 ''' baseline model'''
 def create_baseline(numHiddenUnits, numClasses, lr):   
@@ -57,26 +49,23 @@ inputSize = 2
 numHiddenUnits =15
 numClasses = 2
 maxEpochs = 12
-miniBatchSize =31
+miniBatchSize =10
 
 ''' folder in where to save'''
 file=str(date.today().day) + '-' + str(date.today().month) + '_' + str(numHiddenUnits) + '_' + str(maxEpochs)+ '_' + str(lr)
 if not os.path.isdir(dataset_path + "\\" + file):
     os.mkdir(dataset_path + "\\" + file)
 
-''' definizione della rete'''
-
-# model.build([len(XTrain),len(XTrain[0]),inputSize])
-# model.summary()
 
 ''' Train '''
 kf=4
 n_runs=10
 acc=[]
+
 for z in range(n_runs):
     print('\n--- RUN %d / %d ---\n'% (z+1, n_runs))
     np.random.seed(z)
-    estimator = KerasClassifier(build_fn=create_baseline(numHiddenUnits, numClasses, lr), epochs=maxEpochs, batch_size=miniBatchSize, verbose=0)
+    # estimator = KerasClassifier(build_fn=create_baseline(numHiddenUnits, numClasses, lr), epochs=maxEpochs, batch_size=miniBatchSize, verbose=0)
     kfold = StratifiedKFold(n_splits=kf, shuffle=True, random_state=z)
     cvscores=[]
     for train, test in kfold.split(X, Y):
